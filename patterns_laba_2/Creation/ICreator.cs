9eb8af9549481393ac60a84010fc8dbc4bec640b
AlexShev <bigalex1058@gmail.com>
2022-08-09 14:16:33 +0300

    удалил ненужное

--- patterns_laba_2/Creation/ICreator.cs
+++ patterns_laba_2/Creation/ICreator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace patterns_laba_2.Creation;
+﻿namespace patterns_laba_2.Creation;
 
 /// <summary>
 /// Интерфейс для использования шаблона "Object Pool" <see cref="ObjectPool"/>
